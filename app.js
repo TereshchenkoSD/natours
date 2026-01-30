e5e16f1afcf99ac3b95d25a3fd20c4db1c9e6bbb
TereshchenkoSD <58526730+TereshchenkoSD@users.noreply.github.com>
2026-01-30 21:57:05 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const helmet = require('helmet');
 const AppError = require('./utils/appError');
@@ -26,6 +27,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1)GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+app.options('*', cors());
+
 // Serving static files
 
 app.use(express.static(path.join(__dirname, 'public')));
